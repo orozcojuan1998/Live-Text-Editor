3558ddf868a3a4c35c74c4dda0cfe935fd5215f7
Juan David Orozco <ingjuanorozco@outlook.com>
2019-09-09 13:16:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const log = console.log
 // initialize http server, socket.io and port number
 const http = require('http').createServer()
 const io = require('socket.io')(http)
-http.listen('localhost', () =>)
+http.listen('localhost')
 io.on('connection', (socket) => {
     log('connected')
     socket.on('message', (evt) => {
